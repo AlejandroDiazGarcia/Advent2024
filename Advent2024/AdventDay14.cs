0e77284b875fc5ab60ca07bd2083081196d36634
Alejandro Díaz García <alex@icuk.net>
2024-12-20 11:00:25 +0000

    Day 14 & 15

---
+++ Advent2024/AdventDay14.cs
@@ -0,0 +1,118 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Advent2024 {
+
+    public class AdventDay14 : AdventDay {
+
+        int xLength = 103, yLength = 101;
+
+        public void RunCodePart1(string[] input) {
+
+            int[] quarters = new int[4];
+            int xMiddle = xLength / 2, yMiddle = yLength / 2;
+            List<Robot> robots = GetRobots(input);
+            int seconds = 100;
+
+            for (int i = 0; i < robots.Count; i++) {
+
+                Robot robot = robots[i];
+
+                Move(ref robot, seconds);
+
+                if (robot.x < xMiddle && robot.y < yMiddle) quarters[0]++;
+                else if (robot.x < xMiddle && robot.y > yMiddle) quarters[1]++;
+                else if (robot.x > xMiddle && robot.y < yMiddle) quarters[2]++;
+                else if (robot.x > xMiddle && robot.y > yMiddle) quarters[3]++;
+            }
+
+            Console.WriteLine(quarters[0] * quarters[1] * quarters[2] * quarters[3]);
+        }
+
+        public void RunCodePart2(string[] input) {
+
+            List<Robot> robots = GetRobots(input);
+            int[,] map = new int[xLength, yLength];
+
+            // Pattern starts on second 76, every 103 secs
+            for (int i = 0; i < robots.Count; i++) {
+
+                Robot robot = robots[i];
+
+                Move(ref robot, 76);
+            }
+
+            int seconds = 76, j = 103;
+            while (true) {
+
+                seconds += j;
+
+                Console.WriteLine("Second: " + seconds);
+                map = new int[xLength, yLength];
+
+                for (int i = 0; i < robots.Count; i++) {
+
+                    Robot robot = robots[i];
+
+                    Move(ref robot, j);
+
+                    map[robot.x, robot.y] = 1;
+                }
+
+                // Plot
+                for (int x = 0; x < xLength; x++) {
+                    for (int y = 0; y < yLength; y++) {
+                        Console.Write(map[x, y] == 0 ? " " : "x");
+                    }
+                    Console.WriteLine();
+                }
+
+                Console.WriteLine(Environment.NewLine);
+                Console.WriteLine(Environment.NewLine);
+            }
+        }
+
+        private List<Robot> GetRobots(string[] input) {
+
+            List<Robot> robots = new List<Robot>();
+
+            string pattern = @"p=(\d+),(\d+) v=(-?\d+),(-?\d+)";
+            Match match;
+
+            for (int i = 0; i < input.Length; i++) {
+
+                match = Regex.Match(input[i], pattern);
+
+                Robot robot = new Robot {
+                    y = Convert.ToInt32(match.Groups[1].Value),
+                    x = Convert.ToInt32(match.Groups[2].Value),
+                    vY = Convert.ToInt32(match.Groups[3].Value),
+                    vX = Convert.ToInt32(match.Groups[4].Value),
+                };
+
+                robots.Add(robot);
+            }
+
+            return robots;
+        }
+
+
+        public void Move(ref Robot robot, int seconds = 1) {
+
+            robot.x = (robot.x + (robot.vX * seconds)) % xLength;
+            robot.y = (robot.y + (robot.vY * seconds)) % yLength;
+
+            if (robot.x < 0) robot.x = xLength + robot.x;
+            if (robot.y < 0) robot.y = yLength + robot.y;
+        }
+
+    }
+
+    public class Robot {
+
+        public int x { get; set; }
+        public int y { get; set; }
+
+        public int vX { get; set; }
+        public int vY { get; set; }
+    }
+}
---
+++ Advent2024/AdventDay15.cs
@@ -0,0 +1,251 @@
+﻿namespace Advent2024 {
+
+    public class AdventDay15 : AdventDay {
+
+        /// <summary>
+        /// Input map
+        /// </summary>
+        private char[][] input;
+
+        /// <summary>
+        /// Robot position
+        /// </summary>
+        private Tuple<int, int> robot;
+
+        /// <summary>
+        /// Possible directions
+        /// </summary>
+        private Dictionary<char, Tuple<int, int>> directions = new Dictionary<char, Tuple<int, int>> {
+            {'^', Tuple.Create(-1, 0) },
+            {'v', Tuple.Create(1, 0) },
+            {'>', Tuple.Create(0, 1) },
+            {'<', Tuple.Create(0, -1) },
+        };
+
+        public void RunCodePart1(string[] input) {
+
+            // Initialise values
+            this.input = input.Select(s => s.ToCharArray()).ToArray();
+            int i = 0;
+
+            for (i = 0; i < input.Length; i++) {
+
+                if (input[i] == string.Empty) {
+                    break;
+                }
+
+                if (robot == null) {
+                    // Get robot coords
+                    if (input[i].Contains("@")) robot = Tuple.Create(i, input[i].IndexOf("@"));
+                    else continue;
+
+                }
+            }
+
+            int total = FollowInstructions(i);
+
+            Console.WriteLine(total);
+        }
+
+
+        public void RunCodePart2(string[] input) {
+
+            // Initialise values
+            int i = 0;
+
+
+            // Duplicate values
+            List<char[]> lines = new List<char[]>();
+
+            for (i = 0; i < input.Length; i++) {
+
+                if (input[i] == string.Empty) {
+                    break;
+                }
+
+                List<char> line = new List<char>();
+                for (int j = 0; j < input[i].Length; j++) {
+
+                    if (input[i][j] == 'O') {
+                        line.Add('[');
+                        line.Add(']');
+                    }
+                    else {
+
+                        line.Add(input[i][j]);
+
+                        if (input[i][j] == '@') {
+                            line.Add('.');
+                        }
+                        else {
+                            line.Add(input[i][j]);
+                        }
+                    }
+                }
+                if (line.Contains('@')) robot = Tuple.Create(i, line.IndexOf('@'));
+                lines.Add(line.ToArray());
+
+            }
+
+            // Add instructions with break to match part 1
+            lines.Add(null);
+            for (int j = i + 1; j < input.Length; j++) {
+
+                // Directions
+                lines.Add(input[j].ToCharArray());
+            }
+            this.input = lines.ToArray();
+
+
+            int total = FollowInstructions(i, true);
+
+            Console.WriteLine(total);
+        }
+
+        private int FollowInstructions(int i, bool big = false) {
+
+            for (i = i + 1; i < input.Length; i++) {
+
+                // Directions
+                for (int j = 0; j < input[i].Length; j++) {
+
+                    // Check whether the robot can move his position
+                    if (CanMove(directions[input[i][j]], big)) {
+
+                        // Update old and new position
+                        this.input[robot.Item1][robot.Item2] = '.';
+                        robot = Tuple.Create(robot.Item1 + directions[input[i][j]].Item1, robot.Item2 + directions[input[i][j]].Item2);
+                        this.input[robot.Item1][robot.Item2] = '@';
+
+                        // Print map for debugging
+                        Console.WriteLine($"Movement {i - 11} {j}: {input[i][j]}");
+                        for (int k = 0; k < input.Length; k++) {
+
+                            if (input[k] == null || input[k].Length == 0) break;
+                            Console.WriteLine(input[k]);
+                        }
+                        Console.WriteLine(Environment.NewLine);
+                    }
+                }
+            }
+
+            // Work out total
+            int total = 0;
+            for (i = 0; i < input.Length; i++) {
+
+                if (input[i] == null || input[i].Length == 0) break;
+
+                for (int j = 0; j < this.input[0].Length; j++) {
+                    if (this.input[i][j] == 'O' || this.input[i][j] == '[') {
+                        total += 100 * i + j;
+                    }
+                }
+            }
+
+            return total;
+        }
+
+        private bool CanMove(Tuple<int, int> direction, bool big) {
+
+            // Base case
+            int boxX = robot.Item1 + direction.Item1, boxY = robot.Item2 + direction.Item2;
+            if (input[boxX][boxY] == '.') return true;
+            if (input[boxX][boxY] == '#') return false;
+
+            // Boxes
+            if (!big) {
+
+                while (input[boxX][boxY] == 'O') {
+                    boxX += direction.Item1;
+                    boxY += direction.Item2;
+                }
+
+                // Check if there is a wall at the end of the boxes
+                if (input[boxX][boxY] == '#') return false;
+
+                // Move box
+                input[boxX][boxY] = 'O';
+            }
+            else {
+
+                if (direction.Item1 == 0) {
+
+                    // Moving horizontally
+                    int startY = boxY;
+                    while (input[boxX][boxY] == '[' || input[boxX][boxY] == ']') {
+                        boxY += direction.Item2;
+                    }
+
+                    // Check if there is a wall at the end of the boxes
+                    if (input[boxX][boxY] == '#') return false;
+
+                    // Move boxes
+                    while(boxY != startY) {
+
+                        input[boxX][boxY] = input[boxX][boxY - direction.Item2];
+                        boxY -= direction.Item2;
+                    }
+                }
+                else {
+
+                    // Moving vertically - check where the boxes starts and end on the Y axis     
+                    List<int> boxCoords = new List<int>() { boxY - (input[boxX][boxY] == ']' ? 1 : 0), boxY + (input[boxX][boxY] == '[' ? 1 : 0) };
+                    Dictionary<int, List<int>> boxLevels = new Dictionary<int, List<int>>();
+
+                    // Check until there is no new layer of boxes
+                    while (boxCoords.Count > 0) {
+
+                        // Store box positions on each level
+                        List<int> existingLevel = new List<int>(boxCoords);
+                        boxCoords = new List<int>();
+
+                        foreach (int y in existingLevel) {
+
+                            if (input[boxX][y] == '#') return false;
+
+                            if (input[boxX][y] == ']') {
+                                if(!boxCoords.Contains(y - 1)) boxCoords.Add(y - 1);
+                                if (!boxCoords.Contains(y)) boxCoords.Add(y);
+                            }
+                            else if (input[boxX][y] == '[') {
+                                if (!boxCoords.Contains(y)) boxCoords.Add(y);
+                                if (!boxCoords.Contains(y + 1)) boxCoords.Add(y + 1);
+                            }
+                        }
+
+                        if (boxCoords.Count > 0) {
+
+                            boxLevels.Add(boxX, boxCoords);
+                            boxX += direction.Item1;
+                        }
+                    }
+
+                    // Move boxes
+                    while (boxX - direction.Item1 != robot.Item1) {
+
+                        List<int> previousLevel = boxLevels.First(x => x.Key == boxX - direction.Item1).Value;
+                        List<int> newLevel = boxLevels.FirstOrDefault(x => x.Key == boxX).Value ?? new List<int>();
+
+                        // Set the previous level in the new one
+                        foreach (int y in previousLevel) {
+                            input[boxX][y] = input[boxX - direction.Item1][y];
+                        }
+
+                        // Check if we need to remove boxes on the new level
+                        foreach (int y in newLevel.Except(previousLevel)) {
+                            input[boxX][y] = '.';
+                        }
+
+                        boxX -= direction.Item1;
+                    }
+
+                    // For the square next to the robot, on the new level, replace with a dot
+                    input[robot.Item1 + direction.Item1][boxY + (input[robot.Item1 + direction.Item1][boxY] == '[' ? 1 : -1)] = '.';
+
+                }
+            }
+
+            return true;
+        }
+    }
+}
